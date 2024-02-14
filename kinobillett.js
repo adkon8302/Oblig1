f196ad596360776f622634305ab459f7b11b9b2a
Adrian <adriankongshaug@gmail.com>
2024-02-14 11:16:00 +0100

    endra litt

--- kinobillett.js
+++ kinobillett.js
@@ -1,12 +1,14 @@
 const billettRegistering = [];
 function  kjopBillett() {
 
+       const film = document.getElementById("film").value;
+       const antall = document.getElementById("Antall").value;
        const fornavn = document.getElementById("fornavn").value;
        const etternavn = document.getElementById("etternavn").value;
        const telefon = document.getElementById("telefon").value;
        const epost = document.getElementById("epost").value;
        const billett = {
-           fornavn,etternavn,telefon,epost
+           film,antall,fornavn,etternavn,telefon,epost
        };
     billettRegistering.push(billett);
 
